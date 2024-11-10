3006ad6cde66842f6debe95d33a3544324efde12
H2SO4 <Zainulabideen8@gmail.com>
2024-11-10 14:17:20 +0500

    Main Commit

---
+++ COD BO6 Terminus/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace COD_BO6_Terminus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Set default button texts
+            buttonX.Text = "";
+            buttonY.Text = "";
+            buttonZ.Text = "";
+            textBoxCode.Text = "Please Select All Symbols";
+        }
+
+        private void InitializeComponent()
+        {
+            this.labelInputSymbols = new Label();
+            this.labelX = new Label();
+            this.labelY = new Label();
+            this.labelZ = new Label();
+            this.labelCode = new Label();
+
+            this.buttonX = new Button();
+            this.buttonY = new Button();
+            this.buttonZ = new Button();
+            this.buttonReset = new Button();
+            this.textBoxCode = new TextBox();
+
+            // Form Properties
+            this.Text = "Symbol Selector";
+            this.Size = new System.Drawing.Size(400, 300);
+
+            // Label for Input Symbols
+            labelInputSymbols.Text = "Input Symbols";
+            labelInputSymbols.Location = new System.Drawing.Point(150, 20);
+            labelInputSymbols.Font = new System.Drawing.Font("Arial", 12, System.Drawing.FontStyle.Bold);
+
+            // X Symbol Label
+            labelX.Text = "X Symbol:";
+            labelX.Location = new System.Drawing.Point(50, 60);
+
+            // Y Symbol Label
+            labelY.Text = "Y Symbol:";
+            labelY.Location = new System.Drawing.Point(150, 60);
+
+            // Z Symbol Label
+            labelZ.Text = "Z Symbol:";
+            labelZ.Location = new System.Drawing.Point(250, 60);
+
+            // Buttons for Symbols
+            buttonX.Location = new System.Drawing.Point(50, 90);
+            buttonX.Click += new EventHandler(this.ButtonX_Click);
+
+            buttonY.Location = new System.Drawing.Point(150, 90);
+            buttonY.Click += new EventHandler(this.ButtonY_Click);
+
+            buttonZ.Location = new System.Drawing.Point(250, 90);
+            buttonZ.Click += new EventHandler(this.ButtonZ_Click);
+
+            // Code Label
+            labelCode.Text = "Your Code:";
+            labelCode.Location = new System.Drawing.Point(50, 150);
+
+            // TextBox for Code
+            textBoxCode.Location = new System.Drawing.Point(50, 180);
+            textBoxCode.Width = 300;
+            textBoxCode.ReadOnly = true;
+
+            // Reset Button
+            buttonReset.Text = "Reset All";
+            buttonReset.Location = new System.Drawing.Point(150, 220);
+            buttonReset.Click += new EventHandler(this.ButtonReset_Click);
+
+            // Add Controls to Form
+            this.Controls.Add(labelInputSymbols);
+            this.Controls.Add(labelX);
+            this.Controls.Add(labelY);
+            this.Controls.Add(labelZ);
+            this.Controls.Add(buttonX);
+            this.Controls.Add(buttonY);
+            this.Controls.Add(buttonZ);
+            this.Controls.Add(labelCode);
+            this.Controls.Add(textBoxCode);
+            this.Controls.Add(buttonReset);
+        }
+
+        private void ButtonX_Click(object sender, EventArgs e)
+        {
+            using (var symbolForm = new SymbolSelectorApp())
+            {
+                if (symbolForm.ShowDialog() == DialogResult.OK)
+                {
+                    buttonX.Text = symbolForm.SelectedSymbol;
+                    UpdateCode();
+                }
+            }
+        }
+
+        private void ButtonY_Click(object sender, EventArgs e)
+        {
+            using (var symbolForm = new SymbolSelectorApp())
+            {
+                if (symbolForm.ShowDialog() == DialogResult.OK)
+                {
+                    buttonY.Text = symbolForm.SelectedSymbol;
+                    UpdateCode();
+                }
+            }
+        }
+
+        private void ButtonZ_Click(object sender, EventArgs e)
+        {
+            using (var symbolForm = new SymbolSelectorApp())
+            {
+                if (symbolForm.ShowDialog() == DialogResult.OK)
+                {
+                    buttonZ.Text = symbolForm.SelectedSymbol;
+                    UpdateCode();
+                }
+            }
+        }
+
+
+
+        private void ButtonReset_Click(object sender, EventArgs e)
+        {
+            buttonX.Text = "";
+            buttonY.Text = "";
+            buttonZ.Text = "";
+            textBoxCode.Text = "Please Select All Symbols";
+        }
+
+        private void UpdateCode()
+        {
+            try
+            {
+                int x = int.Parse(buttonX.Text);
+                int y = int.Parse(buttonY.Text);
+                int z = int.Parse(buttonZ.Text);
+                textBoxCode.Text = $"Code: {x * 2 + 11} {(2 * z + y) - 5} {Math.Abs(y + z - x)}";
+            }
+            catch (Exception)
+            {
+                textBoxCode.Text = "Please Select All Symbols";
+
+
+            }
+        }
+
+        private string ShowSymbolDialog()
+        {
+            // This is a simple input dialog for selecting a symbol
+            using (var dialog = new Form())
+            {
+                var inputBox = new TextBox { Width = 100, Location = new System.Drawing.Point(10, 10) };
+                var buttonOK = new Button { Text = "OK", Location = new System.Drawing.Point(120, 10) };
+
+                dialog.Controls.Add(inputBox);
+                dialog.Controls.Add(buttonOK);
+
+                string symbol = null;
+                buttonOK.Click += (s, e) => { symbol = inputBox.Text; dialog.Close(); };
+
+                dialog.ShowDialog();
+                return symbol;
+            }
+        }
+
+        // Declare UI elements
+        private Label labelInputSymbols, labelX, labelY, labelZ, labelCode;
+        private Button buttonX, buttonY, buttonZ, buttonReset;
+        private TextBox textBoxCode;
+    }
+}
---
+++ COD BO6 Terminus/SymbolSelectorApp.Designer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace COD_BO6_Terminus
+{
+    public partial class SymbolSelectorApp : Form
+    {
+        public string SelectedSymbol { get; private set; }
+
+        public SymbolSelectorApp()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.Text = "Select a Symbol";
+            this.Size = new System.Drawing.Size(200, 250);
+
+            // Create PictureBoxes for symbols
+            PictureBox[] symbolBoxes = new PictureBox[6];
+            for (int i = 0; i < symbolBoxes.Length; i++)
+            {
+                symbolBoxes[i] = new PictureBox
+                {
+                    Size = new System.Drawing.Size(50, 50),
+                    Location = new System.Drawing.Point(10 + (i % 3) * 60, 10 + (i / 3) * 60),
+                    BorderStyle = BorderStyle.FixedSingle,
+                    
+                };
+                symbolBoxes[i].Click += SymbolBox_Click;
+                this.Controls.Add(symbolBoxes[i]);
+            }
+
+            // Load images (replace "path_to_image" with actual image paths or resources)
+            symbolBoxes[0].Image = Properties.Resources._0; // Load your image here
+            symbolBoxes[0].Tag = "0";
+            symbolBoxes[1].Image = Properties.Resources._10;
+            symbolBoxes[1].Tag = "10";
+            symbolBoxes[2].Image = Properties.Resources._11;
+            symbolBoxes[2].Tag = "11";
+            symbolBoxes[3].Image = Properties.Resources._22;
+            symbolBoxes[3].Tag = "22";
+            symbolBoxes[4].Image = Properties.Resources._21;
+            symbolBoxes[4].Tag = "21";
+            symbolBoxes[5].Image = Properties.Resources._20;
+            symbolBoxes[5].Tag = "20";
+
+            // Cancel button
+            Button cancelButton = new Button
+            {
+                Text = "Cancel",
+                Location = new System.Drawing.Point(60, 180),
+                Size = new System.Drawing.Size(75, 23)
+            };
+            cancelButton.Click += (s, e) => this.Close();
+            this.Controls.Add(cancelButton);
+        }
+
+        private void SymbolBox_Click(object sender, EventArgs e)
+        {
+            // Set SelectedSymbol to the Tag or identifier of the clicked symbol
+            var pictureBox = sender as PictureBox;
+            SelectedSymbol = pictureBox.Tag.ToString();
+            this.DialogResult = DialogResult.OK; // Close the form with OK result
+            this.Close();
+        }
+    }
+}
